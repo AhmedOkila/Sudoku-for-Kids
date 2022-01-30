dc428d72e4eb21c5e6e6f67f62d81058292935d1
youssefwt <Ywtalaat@gmail.com>
2022-01-30 23:12:58 +0200

    some style updates, +  send background image
    update style and fonts based on game page, added code to send background image along with picture group

--- choosing picture group/getPicPath.js
+++ choosing picture group/getPicPath.js
@@ -1,23 +1,31 @@
 "use strict"
 
-let image = document.getElementById("sora")
+let image = document.getElementById("sora");
+let div = document.getElementById('div');
 let picGroup = localStorage.getItem('Group');
+let backGround;
 let theme;
 switch (picGroup){
-    case "rocket ships":
+    case "rocket_ships":
     case "aliens":
-        theme = 'space' 
+        theme = 'space_'
+        backGround = 'spacebg.jpg'
         break;
     case "Fish":
     case "boats":
-        theme = 'ocean'
+        theme = 'ocean_'
+        backGround = 'ocean.jpg'
         break;
 }
 
 //note that this path is from another folder, from main start with one . instead of two ..
-let path = "../images/" + theme + " theme/"+picGroup+"/" +"3.PNG";
-console.log(path);
+let picPath = "../images/" + theme + "theme/"+picGroup+"/" +"2.PNG";
+let bgPath = "../images/" + theme + "theme/" + backGround;
+
+console.log(picPath);
+console.log(bgPath);
 
 
-image.src=path;
+image.src=picPath;
+div.style.backgroundImage = `url(${bgPath})`;
 // console.log(image);
